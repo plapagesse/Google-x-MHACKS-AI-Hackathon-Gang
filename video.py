3eae068f85c27732ef6df096107ca5fd1e857600
Vara Arul <varaarul@umich.edu>
2024-04-13 20:32:10 -0400

    meeting productivity prompt

--- video.py
+++ video.py
@@ -7,6 +7,7 @@ import torch
 from dotenv import load_dotenv
 import pdb
 import pandas as pd
+import pickle
 
 ### CONFIGURE KEYS IN A .ENV FILE###
 load_dotenv()
@@ -34,7 +35,17 @@ class MeetingFeedback:
     def __init__(self):
         pass
     def prompt(self, frames, audio, speaker_frequencies):
-        pass
+        prompt = "You have been provided with timestamped image frames and an audio file of a recent meeting, along with a dictionary detailing speaker frequencies. Analyze the content of the discussion to determine the proportion of time spent on relevant versus unrelated topics. Identify and quantify moments where the discussion veers off-topic to assess overall meeting productivity"
+    # #prompt = "What was the first thing said in the meeting?"
+    # # Set the model to Gemini 1.5 Pro.
+        model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
+    # Make the LLM request.
+        request = make_request(prompt, frames)
+        request.append(audio)
+        request.append(str(speaker_frequencies))
+        response = model.generate_content(request,
+                                    request_options={"timeout": 600})
+        return response
 
 def get_timestamp(filename):
   """Extracts the frame count (as an integer) from a filename with the format
@@ -80,8 +91,8 @@ def extract_frame_from_video(video_file_path):
 def make_request(prompt, files):
   request = [prompt]
   for file in files:
-    request.append(file.timestamp)
-    request.append(file.response)
+    request.append(get_timestamp(file.display_name))
+    request.append(file)
   return request
 
 def upload_video(prefix, df, do_upload):
@@ -168,7 +179,7 @@ def get_frames_and_audio(test_file):
   df = df[df['origin_name'] == test_file]
 
   frame_uids = df[df['display_name'].str.contains('jpg')]['uid'].to_list()
-  audio_uid = df[df['display_name'].str.contains('mp3')]['uid'].iloc[0].to_list()
+  audio_uid = df[df['display_name'].str.contains('mp3')]['uid'].iloc[0]
 
   frames = []
   for frame in frame_uids:
@@ -177,6 +188,32 @@ def get_frames_and_audio(test_file):
   audio = genai.get_file(audio_uid)
 
   return frames, audio
+
+def get_speaker_frequencies(audio_file):
+  # .wav file is passed in
+  pass
+
+def get_frames():
+
+  # Specify the path of your pickle file
+  pickle_file_path = 'file_handles.pkl'
+
+  # Open the file in binary read mode
+  with open(pickle_file_path, 'rb') as file:
+      # Deserialize the pickled list of objects
+      loaded_objects = pickle.load(file)
+  
+
+  return loaded_objects
+
+def get_audio(test_file):
+  df = pd.read_csv('file_manifest.csv')
+
+  df = df[df['origin_name'] == test_file]
+  audio_uid = df[df['display_name'].str.contains('mp3')]['uid'].iloc[0]
+  audio = genai.get_file(audio_uid)
+
+  return audio
 
 
 
@@ -187,13 +224,23 @@ if __name__ == '__main__':
     # prefixes = ['testSample1', 'testSample2', 'testSample3', 'testSample4', 'testSample5']
     # for prefix in prefixes:
     #    df = upload_video(prefix, df, do_upload=genai.upload_file)
+    #df.to_csv("file_manifest.csv")
 
-    frames, audio = get_frames_and_audio("testSample1")
+
+    #frames, audio = get_frames_and_audio("testSample1")
+    frames = get_frames()
+    audio = get_audio("testSample1")
     print(frames)
     print(audio)
+
+    meeting_feedback = MeetingFeedback()
+
+    response = meeting_feedback.prompt(frames, audio, "{'SPEAKER_00': 60, 'SPEAKER_01': 30, 'SPEAKER_02': 49}")
+    print(response.text)
+
     
 
-    #df.to_csv("file_manifest.csv")
+
     ### GEMINI CALL ###
     # Create the prompt.
     # prompt = "based on whose name is highlighted in the meeting (on right side of screen) at each point in the audio file, determine accurately who speaks a lot and who does not. name lighting up = speaking. also give feedback for this meeting. you have to give feedback to each member who spoke, level of participation, as well as overall meeting productivity. also note important things discussed, and possible future tasks"
